aa9ff6e6bd912c4fd1c04e5849b3009604281398
Kshitij Sharma <sharmakshitij535@gmail.com>
2023-04-03 18:01:18 +0530

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NotesRoundedIcon from "@mui/icons-material/NotesRounded";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NotesRoundedIcon />
+        Instant Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
